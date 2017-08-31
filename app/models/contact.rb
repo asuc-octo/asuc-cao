7e8b117450f82cc3c421b9bd20724a64c55ded32
Hannah He <hahe@berkeley.edu>
2017-08-30 22:13:18 -0700

    First commit

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,11 +2,12 @@ class Contact
   include ActiveModel::Model
   attr_accessor :name, :string
   attr_accessor :email, :string
-  attr_accessor :content, :string
+  attr_accessor :password, :string
+  attr_accessor :meetings, :string
 
   validates_presence_of :name
   validates_presence_of :email
-  validates_presence_of :content
+  validates_presence_of :meetings
   validates_format_of :email,
     with: /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
   validates_length_of :content, :maximum => 500
